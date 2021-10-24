071a411c2884ba16fa39a237fec58224a5d33934
Daniel <danielac@protonmail.com>
2021-10-23 18:48:00 -0600

    Uses "if __name__ == '__main__'" setup so functions can be imported by other Python files

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ Handle special characters entered in names by user so script won't attempt to ma
 
 If LastYearMatches.txt doesn't exist, create it, prompt user to edit it, tell them they can paste from a spreadsheet, & use tab delimited file
 
-Use "if __name__ == '__main__'" setup
-
 Continue asking for player list if duplicates are found or there is an insufficient number of players
 
 Keep any 2 players from being each other's givers & receivers.
@@ -14,26 +12,13 @@ Requires 5 players minimum?
 
 import os, random, sys, datetime
 
-version = "0.0.2"
-
-## GLOBAL VARIABLES ##
-'''
-List of test givers to enter in prompt:
-Trogdor, Satoshi Nakamoto, Aragorn, Nacho Libre, Li'l Sebastian, Groot, Brave Sir Robin
-'''
-
-givers = {}
-lastMatches = {}
-giversWithFinalReceivers = {}
-
-
 ## FUNCTIONS ##
-def PopulateGiversDictionary(validatedPlayerList):
+def PopulateGiversDictionary(givers, validatedPlayerList):
 	for player in validatedPlayerList:
 			givers[player] = []
 
 
-def CheckGiverLengths():
+def CheckGiverLengths(givers):
 	activeGiver = ''
 	for giver, receivers in givers.items():
 		if len(receivers) < 2:
@@ -41,7 +26,7 @@ def CheckGiverLengths():
 	return activeGiver
 
 
-def AssignInitialReceivers(giver):
+def AssignInitialReceivers(giver, givers, validatedPlayerList, lastMatches):
 	# Assign full list of players to each giver
 	for player in validatedPlayerList:
 		givers[giver].append(player)
@@ -54,7 +39,7 @@ def AssignInitialReceivers(giver):
 			givers[giver].remove(lastMatches[giver])
 
 
-def ChooseReceiver(activeGiver):
+def ChooseReceiver(activeGiver, givers, giversWithFinalReceivers):
 	if len(givers[activeGiver]) > 0:
 		activeReceiver = random.choice(givers[activeGiver])
 	# Add giver & receiver pair to giversWithFinalReceivers{} then delete giver from givers{}
@@ -66,105 +51,121 @@ def ChooseReceiver(activeGiver):
 			receiverList.remove(activeReceiver)
 
 
-def Restart():
-	PopulateGiversDictionary(validatedPlayerList)
+def Restart(givers, validatedPlayerList, lastMatches):
+	PopulateGiversDictionary(givers, validatedPlayerList)
 	for giver in givers:
-		AssignInitialReceivers(giver)
+		AssignInitialReceivers(giver, givers, validatedPlayerList, lastMatches)
 
 
 
 
 
-## SETUP ##
-print('Secret Santa Script version ' + version)
+def main():
+	## SETUP ##
+	version = "0.0.2"
+	givers = {}
+	lastMatches = {}
+	giversWithFinalReceivers = {}
 
-# Populate lastMatches from LastYearMatches.txt
-with open('LastYearMatches.txt') as file:
-	lines = file.readlines()
-	for line in lines:
-		if not line.startswith('#') and not line.strip() == (''):
-			oldMatch = line.strip().split(':')
-			giver = oldMatch[0]
-			receiver = oldMatch[1]
-			lastMatches[giver] = receiver
+	print('Secret Santa Script version ' + version)
 
 
-# Get & validate player list
-playerList = input('Enter list of people participating separated by commas: ').split(',')
-validatedPlayerList = []
-# Add players from playerList to validatedPlayerList that aren't empty strings or duplicates
-for player in playerList:
-	player = player.strip()
-	if player in validatedPlayerList:
-		input(f'Duplicate player found: {player}. Please type "Enter" to exit then re-run script')
-		sys.exit()
-	else:
-		if player != '':
-			validatedPlayerList.append(player)
-totalPlayers = len(validatedPlayerList)
-if totalPlayers < 4:
-	input(f'A minimum of 4 players is required, & you entered {totalPlayers}. Please type "Enter" to exit then re-run script')
-	sys.exit()
+	# Populate lastMatches from LastYearMatches.txt
+	with open('LastYearMatches.txt') as file:
+		lines = file.readlines()
+		for line in lines:
+			if not line.startswith('#') and not line.strip() == (''):
+				oldMatch = line.strip().split(':')
+				giver = oldMatch[0]
+				receiver = oldMatch[1]
+				lastMatches[giver] = receiver
 
 
-PopulateGiversDictionary(validatedPlayerList)
-
-
-# Add all giver names to each giver then remove their own name & their last year receiver
-for giver in givers:
-	AssignInitialReceivers(giver)
-
-
-
-## CHOOSE FINAL RECEIVERS ##
-while True:
-	# If any giver has less than 3 receivers, they're set as the active giver. Otherwise, activeGiver returns as ''
-	activeGiver = CheckGiverLengths()
-	if activeGiver == '':
-		activeGiver = random.choice(list(givers.keys()))
-	# The last giver will occasionally not have any receiver
-	# options left due to the randomized choosing of
-	# givers and receivers.
-	# When this happens, the assignment process restarts
-	try:
-		ChooseReceiver(activeGiver)
-	except:
-		Restart()
-	# Check length of givers{} to see if all have been assigned receivers
-	if len(givers.keys()) == 0:
-		break
-
-
-
-## CREATE & EDIT FILES ##
-# Updates LastYearMatches.txt with this year's matches in
-# preparation for next year
-with open('LastYearMatches.txt', 'w') as file:
-	 print('# IMPORTANT. If you edit this file, you must follow the following syntax: <giver name>:<receiver name>\n', file=file)
-	 for giver, receiver in giversWithFinalReceivers.items():
-		 print(f'{giver}:{receiver}', file=file)
-
-
-# Creates one file per giver. Each file is named after a giver & contains their receiver's name
-matchesDir = 'Matches-' + datetime.datetime.today().strftime("%Y-%b-%d")
-# If a "Matches-<YYYYMMMDD>" dir exists, create another one
-# ending with "_<i>". The value of i depends on what
-# additional "_<i>" files already exist
-if os.path.exists(matchesDir):
-	i = 2
-	while True:
-		dupMatchesDir = matchesDir + '_' + str(i)
-		if os.path.exists(dupMatchesDir):
-			i += 1
+	''' Test givers to enter in prompt:
+	Trogdor, Satoshi Nakamoto, Aragorn, Nacho Libre, Li'l Sebastian, Groot, Brave Sir Robin
+	'''
+	# Get & validate player list
+	playerList = input('Enter list of people participating separated by commas: ').split(',')
+	validatedPlayerList = []
+	# Add players from playerList to validatedPlayerList that aren't empty strings or duplicates
+	for player in playerList:
+		player = player.strip()
+		if player in validatedPlayerList:
+			input(f'Duplicate player found: {player}. Please type "Enter" to exit then re-run script')
+			sys.exit()
 		else:
-			os.mkdir(dupMatchesDir)
-			matchesDir = dupMatchesDir
-			break
-else:
-	os.mkdir(matchesDir)
-	
-for giver, receiver in giversWithFinalReceivers.items():
-	 with open(f'{matchesDir}\\{giver}.txt', 'w') as file:
-		 print(f'You are buying for {receiver}', file=file)
+			if player != '':
+				validatedPlayerList.append(player)
+	totalPlayers = len(validatedPlayerList)
+	if totalPlayers < 4:
+		input(f'A minimum of 4 players is required, & you entered {totalPlayers}. Please type "Enter" to exit then re-run script')
+		sys.exit()
 
-print('\nFinished!')
+
+
+	## SCRIPT BODY ##
+	PopulateGiversDictionary(givers, validatedPlayerList)
+
+
+	# Add all giver names to each giver then remove their own name & their last year receiver
+	for giver in givers:
+		AssignInitialReceivers(giver, givers, validatedPlayerList, lastMatches)
+
+
+	# Choose final receivers
+	while True:
+		# If any giver has less than 3 receivers, they're set as the active giver. Otherwise, activeGiver returns as ''
+		activeGiver = CheckGiverLengths(givers)
+		if activeGiver == '':
+			activeGiver = random.choice(list(givers.keys()))
+		# The last giver will occasionally not have any receiver
+		# options left due to the randomized choosing of
+		# givers and receivers.
+		# When this happens, the assignment process restarts
+		try:
+			ChooseReceiver(activeGiver, givers, giversWithFinalReceivers)
+		except:
+			Restart(givers, validatedPlayerList, lastMatches)
+		# Check length of givers{} to see if all have been assigned receivers
+		if len(givers.keys()) == 0:
+			break
+
+
+	# Update LastYearMatches.txt with this year's matches in
+	# preparation for next year
+	with open('LastYearMatches.txt', 'w') as file:
+		print('# IMPORTANT. If you edit this file, you must follow the following syntax: <giver name>:<receiver name>\n', file=file)
+		for giver, receiver in giversWithFinalReceivers.items():
+			print(f'{giver}:{receiver}', file=file)
+
+
+	# Creates one file per giver. Each file is named after a giver & contains their receiver's name
+	matchesDir = 'Matches-' + datetime.datetime.today().strftime("%Y-%b-%d")
+	# If a "Matches-<YYYYMMMDD>" dir exists, create another one
+	# ending with "_<i>". The value of i depends on what
+	# additional "_<i>" files already exist
+	if os.path.exists(matchesDir):
+		i = 2
+		while True:
+			dupMatchesDir = matchesDir + '_' + str(i)
+			if os.path.exists(dupMatchesDir):
+				i += 1
+			else:
+				os.mkdir(dupMatchesDir)
+				matchesDir = dupMatchesDir
+				break
+	else:
+		os.mkdir(matchesDir)
+		
+	for giver, receiver in giversWithFinalReceivers.items():
+		with open(f'{matchesDir}\\{giver}.txt', 'w') as file:
+			print(f'You are buying for {receiver}', file=file)
+
+	print('\nFinished!')
+
+
+
+# Run main() if file is being run rather than imported as a module
+# Allows functions to be imported to other Python files 
+if __name__ == '__main__':
+	main()
